91a793b5bfab37f64dcd923f069b96a3c613a6ae
Tagir <tagir2911@mail.ru>
2023-05-08 16:11:14 +0300

    DB and some request

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class CryptoCurrency(models.Model):
+    name = models.CharField(max_length=40, default="UFO")
+    time = models.TimeField()
+    open = models.DecimalField(max_digits=20, decimal_places=8)
+    high = models.DecimalField(max_digits=20, decimal_places=8)
+    low = models.DecimalField(max_digits=20, decimal_places=8)
+    close = models.DecimalField(max_digits=20, decimal_places=8)
+
+    def __str__(self):
+        return self.time
