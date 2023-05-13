9581748648cb8cbbb433211eb6d19811ec67f288
DamirGuzaerov <guzaerov.damir49@gmail.com>
2023-05-13 18:51:02 +0500

    [add] database updating scheduler

--- api/management/commands/start.py
+++ api/management/commands/start.py
@@ -1,12 +1,13 @@
 from django.core.management.commands.runserver import Command as RunServerCommand
-from api.data_seeder import start
+
+from data_seeder.data_seeder import update_database
 
 
 class Command(RunServerCommand):
     help = "Starts the development server with database update running in the background."
 
     def handle(self, *args, **options):
-        start()
+        update_database()
 
         # call the original runserver command
         super().handle(*args, **options)
--- api/models.py
+++ api/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class CoinMarketInfo(models.Model):
-    current_price = models.DecimalField(max_digits=20, decimal_places=8, null=True)
-    market_cap = models.DecimalField(max_digits=20, decimal_places=8, null=True)
-    total_volume = models.DecimalField(max_digits=20, decimal_places=8, null=True)
-    price_change_24h = models.DecimalField(max_digits=20, decimal_places=8, null=True)
-    market_cap_change_24h = models.DecimalField(max_digits=20, decimal_places=8, null=True)
-    circulating_supply = models.DecimalField(max_digits=20, decimal_places=8, null=True)
+    name = models.CharField(max_length=40)
+    current_price = models.DecimalField(max_digits=128, decimal_places=8)
+    market_cap = models.DecimalField(max_digits=128, decimal_places=8)
+    total_volume = models.DecimalField(max_digits=128, decimal_places=8)
+    price_change_24h = models.DecimalField(max_digits=128, decimal_places=8)
+    market_cap_change_24h = models.DecimalField(max_digits=128, decimal_places=8)
+    circulating_supply = models.DecimalField(max_digits=128, decimal_places=8)
     last_updated = models.TimeField()
 
 
@@ -16,7 +17,7 @@ class Coin(models.Model):
     symbol = models.CharField(max_length=18)
     image = models.CharField(max_length=256)
 
-    marketInfo = models.OneToOneField(CoinMarketInfo, on_delete=models.CASCADE)
+    market_info = models.ForeignKey(CoinMarketInfo, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
@@ -24,10 +25,10 @@ class Coin(models.Model):
 
 class CoinOHLC(models.Model):
     time = models.TimeField()
-    open = models.DecimalField(max_digits=64, decimal_places=8)
-    high = models.DecimalField(max_digits=64, decimal_places=8)
-    low = models.DecimalField(max_digits=64, decimal_places=8)
-    close = models.DecimalField(max_digits=64, decimal_places=8)
+    open = models.DecimalField(max_digits=128, decimal_places=8)
+    high = models.DecimalField(max_digits=128, decimal_places=8)
+    low = models.DecimalField(max_digits=128, decimal_places=8)
+    close = models.DecimalField(max_digits=128, decimal_places=8)
 
     coin = models.ForeignKey(Coin, on_delete=models.CASCADE)
 
