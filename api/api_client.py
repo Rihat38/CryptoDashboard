a41867ba0bdb606622a9811a5a86e5d32f013204
DamirGuzaerov <guzaerov.damir49@gmail.com>
2023-05-13 21:12:21 +0500

    [update] drop database tables and update currencies page

--- api/api_client.py
+++ api/api_client.py
@@ -16,13 +16,25 @@ def get_currency_for_days(coin_id, vs_currency, days):
     prices = data['prices']
 
 def get_coin_market_data(coin_ids, vs_currency):
-    coin_ids_str = ",".join(coin_ids)
-    url = f"https://api.coingecko.com/api/v3/coins/markets?vs_currency={vs_currency}&ids={coin_ids_str}"
+    url = f"https://api.coingecko.com/api/v3/coins/markets?vs_currency={vs_currency}"
     response = requests.get(url)
     data = response.json()
 
     if response.status_code == 200:
         return data
+    else:
+        print(f"Ошибка при получении данных: {response.status_code}")
+        return None
+
+
+def get_coin_ids():
+    url = "https://api.coingecko.com/api/v3/coins/list"
+    response = requests.get(url)
+    data = response.json()
+    print(data)
+    result = [el["id"] for el in data]
+    if response.status_code == 200:
+        return result
     else:
         print(f"Ошибка при получении данных: {response.status_code}")
         return None
--- api/apps.py
+++ api/apps.py
@@ -5,7 +5,3 @@ class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
 
-    def ready(self):
-        from data_seeder import updater
-        updater.start()
-
